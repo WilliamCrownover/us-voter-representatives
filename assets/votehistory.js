7ae39d051e032f663f4c518876949156f46e87eb
grudgecat <grudgecat@protonmail.com>
2021-06-19 17:08:05 -0700

    adjusted function to add data for voter history card to include accordian collapsible feature

--- assets/votehistory.js
+++ assets/votehistory.js
@@ -1,40 +1,61 @@
-// console.log(Candidate.voteHistoryCard.votes[0]); 
-
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
 
 var displayVotes = setInterval(function() {
         if(apiReturns.length >= 8) {
             clearInterval(displayVotes);
-                appendVotesData();
-                appendExplanationsData();
+            writeVoterInfoCard();
+                // appendVotesData(); 
+                // appendExplanationsData(); 
         }
     }, 500);
-
-
 
 function appendVotesData() {
     for(i = 0; i < Candidate.voteHistoryCard.votes.length; i++) {
         $("#vote-history-content").append(`
-        <output>Date of Vote: ${Candidate.voteHistoryCard.votes[i].date}</output>
-        <output>Bill Number: ${Candidate.voteHistoryCard.votes[i].billID}</output>
-        <output>Title: ${Candidate.voteHistoryCard.votes[i].title}</output>
-        <output>Description: ${Candidate.voteHistoryCard.votes[i].description}</output>
-        <output>Question: ${Candidate.voteHistoryCard.votes[i].question}</output>
-        <output>Position: ${Candidate.voteHistoryCard.votes[i].position}</output>
-        <output>Result: ${Candidate.voteHistoryCard.votes[i].result}</output>
-        <output>Total Yes Votes: ${Candidate.voteHistoryCard.votes[i].totalYes}</output>
-        <output>Total No Votes: ${Candidate.voteHistoryCard.votes[i].totalNo}</output>
-        <output>Total Present: ${Candidate.voteHistoryCard.votes[i].totalPresent}</output>
-        <output>Total Non Votes: ${Candidate.voteHistoryCard.votes[i].totalNotVoting}</output>
-        `)
+        <ul class="collapsible" data-collapsible="accordion">
+            <li>
+                <div class="collapsible-header" style="font-weight:bold">Vote History</div>
+                <div class="collapsible-body">
+                    <p><span style="font-weight:bold">Date of Vote: </span>${Candidate.voteHistoryCard.votes[i].date}</p>
+                    <p><span style="font-weight:bold">Bill Number: </span>${Candidate.voteHistoryCard.votes[i].billID}</p>
+                    <p><span style="font-weight:bold">Title: </span>${Candidate.voteHistoryCard.votes[i].title}</p>
+                    <p><span style="font-weight:bold">Description: </span>${Candidate.voteHistoryCard.votes[i].description}</p>
+                    <p><span style="font-weight:bold">Question: </span>${Candidate.voteHistoryCard.votes[i].question}</p>
+                    <p><span style="font-weight:bold">Position: </span>${Candidate.voteHistoryCard.votes[i].position}</p>
+                    <p><span style="font-weight:bold">Result: </span>${Candidate.voteHistoryCard.votes[i].result}</p>
+                    <p><span style="font-weight:bold">Total Yes Votes: </span>${Candidate.voteHistoryCard.votes[i].totalYes}</p>
+                    <p><span style="font-weight:bold">Total No Votes: </span>${Candidate.voteHistoryCard.votes[i].totalNo}</p>
+                    <p><span style="font-weight:bold">Total Present: </span>${Candidate.voteHistoryCard.votes[i].totalPresent}</p>
+                    <p><span style="font-weight:bold">Total Non Votes: </span>${Candidate.voteHistoryCard.votes[i].totalNotVoting}</p>
+                </div>
+            </li>
+        </ul>
+        `);
     }
+    // $("#vote-history-content").find(".collapsible").collapsible(); 
 }
 
 function appendExplanationsData() {
     for(i = 0; i < Candidate.voteHistoryCard.explanations.length; i++) {
         $("#vote-history-content").append(`
-        <output>Category: ${Candidate.voteHistoryCard.explanations[i].category}</output>
-        <output>Date of Vote: ${Candidate.voteHistoryCard.explanations[i].date}</output>
-        <output>Explanation given: ${Candidate.voteHistoryCard.explanations[i].text}</output>
+        <ul class="collapsible" data-collapsible="accordion">
+            <li>
+                <div class="collapsible-header" style="font-weight:bold">Missed Votes</div>
+                <div class="collapsible-body">
+                <p><span style="font-weight:bold">Category: </span>${Candidate.voteHistoryCard.explanations[i].category}</p>
+                <p><span style="font-weight:bold">Date of Vote: </span>${Candidate.voteHistoryCard.explanations[i].date}</p>
+                <p><span style="font-weight:bold">Explanation given: </span>${Candidate.voteHistoryCard.explanations[i].text}</p>
+                </div>
+            </li>
+        </ul>
         `)
     }
+}
+
+function writeVoterInfoCard() {
+    appendVotesData(); 
+    appendExplanationsData(); 
+    $("#vote-history-content").find(".collapsible").collapsible();
 }
