f9b303a5cab9d1c4d03960f2adecccd1cd62b823
William Crownover <williamcrownover1@gmail.com>
2021-06-19 22:38:21 -0700

    Adjusted padding of percentage chart

--- assets/finance.js
+++ assets/finance.js
@@ -12,8 +12,8 @@ function displayFinances(data) {
     financeDataContainerEl.append(`
         <ul class="collapsible" data-collapsible="accordion">
             <li>
-                <div class="collapsible-header">Percentage of Contributions</div>
-                <div class="collapsible-body">
+                <div class="collapsible-header" style="font-weight:bold">Percentage of Contributions</div>
+                <div class="collapsible-body" style="padding: 15px">
                     <div style="background-color: yellow; padding: 3px 5px; width: ${100-(data.grassRootsPercent())}%">Comittee<br> ${100-(data.grassRootsPercent())}%</div>
                     <div style="background-color: green; padding: 3px 5px; width: ${data.grassRootsPercent()}%">Independent<br> ${data.grassRootsPercent()}%</div>
                 </div>
@@ -21,18 +21,18 @@ function displayFinances(data) {
         </ul>
         <ul class="collapsible" data-collapsible="accordion">
             <li>
-                <div class="collapsible-header">Total Raised: ${formatter.format(data.totalRaised)}</div>
+                <div class="collapsible-header"><span style="font-weight:bold">Total Raised:&#160</span>${formatter.format(data.totalRaised)}</div>
                 <div class="collapsible-body">
-                    <p>Comittee: ${formatter.format(data.nonIndependentContributions())}</p>
-                    <p>Independent: ${formatter.format(data.independentContributions)}</p>
+                    <p><span style="font-weight:bold">Comittee:&#160</span>${formatter.format(data.nonIndependentContributions())}</p>
+                    <p><span style="font-weight:bold">Independent:&#160</span>${formatter.format(data.independentContributions)}</p>
                 </div>
             </li>
         </ul>
-        <p style="margin-left: 15px">Expenditure: ${formatter.format(data.expenditure)}</p>
+        <p style="margin-left: 15px"><span style="font-weight:bold">Expenditure:&#160</span>${formatter.format(data.expenditure)}</p>
         <ul class="collapsible" data-collapsible="accordion">
             <li>
-                <div class="collapsible-header">Current War Chest: ${formatter.format(data.currentStash)}</div>
-                <div class="collapsible-body">Net Gain or Loss: ${formatter.format(data.netGainLoss())}</div>
+                <div class="collapsible-header"><span style="font-weight:bold">Current War Chest:&#160</span>${formatter.format(data.currentStash)}</div>
+                <div class="collapsible-body"><span style="font-weight:bold">Net Gain or Loss:&#160</span>${formatter.format(data.netGainLoss())}</div>
             </li>
         </ul>
     `)
