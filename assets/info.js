61613aeabab650e644d5a719187b07986cb377ef
Kenny Nguyen <kennynguyen@Kennys-MacBook-Pro.local>
2021-06-21 18:24:20 -0700

    additional modifications to info-card js

---
+++ assets/info.js
@@ -0,0 +1,32 @@
+function displayInfoCard (infoCard) {
+
+var infoCard$ = $('#info-card')
+
+
+
+
+
+
+
+
+let info =  ('<div class="card" id="info-card">
+            <div class="card-content">
+                <img src="./assets/images/education_icon_142625.png" alt="info" width="100" height="100">
+                <p>Information Card</p>
+            </div>
+            
+            <div class="table">
+                <div class="info-photo">
+                    <p>Photo</p>
+                </div>
+                
+                <div class="expend-table">
+                    <output>Name</output>
+                    <output>Seat</output>
+                    <output>Party</output>
+                    <output>Contact Link</output>
+                </div>
+            </div>
+        </div>')
+
+}
