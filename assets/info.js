2ee8166bcb3b41896b94c8173b6d02b6f22eb418
Kenny Nguyen <kennynguyen@Kennys-MacBook-Pro.local>
2021-06-22 16:15:43 -0700

    update info.js and removed errors with head

--- assets/info.js
+++ assets/info.js
@@ -8,27 +8,23 @@ var infoCardDataId= $("#infoCardDataContainer");
 
     infoCardDataId.empty();
       infoCardDataId.append(`
-      <div class="card" id="info-card">
+         <div class="card" id="info-card">
 
-                <div class="card-content" >
-                    <img src="./assets/images/education_icon_142625.png" alt="info" width="100" height="100">
-                    <p>Information Card</p>
-                </div>
-
-                <div class="table">
-                    
-                    <div class="expend-table" id=infoCardDataContainer class="collapsible" data-collapsible="accordion">
-                         <output>Name:${Candidate.infoCard.fullName()}</output>
-                         <output>Seat:${Candidate.infoCard.seat()}</output>
-                         <output>Party:${Candidate.infoCard.party()}</output>
-                    </div>
+            <div class="table">
+                
+                <div class="expend-table" id=infoCardDataContainer class="collapsible" data-collapsible="accordion">
+                        <output>Name:${Candidate.infoCard.fullName()}</output>
+                        <output>Seat:${Candidate.infoCard.seat()}</output>
+                        <output>Party:${Candidate.infoCard.party()}</output>
                 </div>
             </div>
+        </div>
             `)
 
      $("#infoCardDataContainer > .collapsible").collapsible();  
 
-    
+
+     console.log(Candidate.infoCard.firstName)
  }
 
      // displayInfoCard(Candidate.infoCard.fullName());
