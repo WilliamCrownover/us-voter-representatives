c1b3782402d3311ac07d7e39aaf5482ad84330b6
grudgecat <grudgecat@protonmail.com>
2021-06-22 00:26:43 -0700

    added findmyrep.js file to get user district info

--- assets/findmyrep.js
+++ assets/findmyrep.js
@@ -1,3 +1,4 @@
+// GLOBAL VARIABLES
 var API_KEY = "AIzaSyDWoNvuofpIPra70zQ42CMuJJJMCx1NzKc";
 var zipCode = "";
 var gapiCivicsURL = "";
@@ -7,20 +8,17 @@ var numberString;
 var district = 0;
 var parsedAddress = "";
 
-// console.log(gapiCivicsURL);
+//Event handler for form submit to get user address info
+//parse address info to prep for API call
+$("#usrAddressForm").submit(function(event) {
+    event.preventDefault();
+    var addr = $("#street_address").val().replace(/\s/g,"%20");
+    var zippy = $("#zip").val();
+    parsedAddress = addr + ", " +zippy;
+    getData(parsedAddress);
+  });
 
-function handleDistrictSearch(event) {
-  event.preventDefault();
-  console.log("userclickregistered");
-  var addr = $("#street_address").val().replace(/\s/g,"%20");
-  var zippy = $("#zip").val();
-  parsedAddress = addr + ", " +zippy;
-  console.log("parsed address is: " + parsedAddress);
-  getData(parsedAddress);
-}
-
-$("#usrAddressForm").on("submit", handleDistrictSearch);
-
+//Use Google Civics API to find user district number from address data
 function getData(address) {
     var foundAt=0;
   gapiCivicsURL = `https://civicinfo.googleapis.com/civicinfo/v2/representatives?includeOffices=true&key=${API_KEY}&address=${address}`;
@@ -34,28 +32,30 @@ function getData(address) {
         }
       })
       .then(function (data) {
+        //due to changing object keys, must convert keys to array
         keysArr = Object.keys(data.divisions);
         for(i=0; i < keysArr.length; i++) {
             myString = keysArr[i];
+            //due to changing data, must search each value to find district key
             if(myString.search("/cd:") !== -1) {
                 foundAt = i;
             }
         }
+        //must convert to actual string object
         myString = keysArr[foundAt];
+        //get district by splitting resulting string at 'cd:'
         district = myString.split("/cd:")[1];
         console.log(district);
       });
 }
 
+//output district result to page
 var readyToGo = setInterval(function() {
   if(keysArr.length > 3) {
     clearInterval(readyToGo);
-    $("#districtOutput").append(`<li>Your District Number is:
-    <span style="font-size: 36px; font-weight:bold">${district}</span></li>`);
-    console.log(district);
+    // $("#districtOutput").empty(); 
+    $("#districtOutput").append(`<li style="list-style-type: none;">Your District Number is:
+    <span style="font-size: 48px; font-weight:bold">${district}</span></li>`);
     district = 0;
-  }
-  else {
-    console.log("error - keys array not complete in fxn setinterval");
   }
 })
