464a358e8b1b3b72cbce4f933e7073e0e530a9b9
Sam <daysbiii@hotmail.com>
2021-06-22 19:37:02 -0700

    Updates to supporters card

--- assets/supporters.js
+++ assets/supporters.js
@@ -1,45 +1,33 @@
 function displaySupporters(data) {
-    $("#supporters-card").empty();
-    $("#supporters-card").append(`
+    $("#supporters-data").empty();
+    $("#supporters-data").append(`
 
     <div class="collapsible-header"><span style= "font-weight:bold; color:#223E88"><h4>supportersCard</h4></span>
 `);
 
-var totalSpent = 0.00;
+
 
 for( var i = 0; i < Candidate.supportersCard.support.length; i++) {
-    if(Candidate.supportersCard.support[i].committeeName.totalSpent == 0.00) {
-console.log(Candidate.supportersCard.support[i]);
-  
-    }
-     else {
-        (Candidate.supportersCard.support[i].committeeName.totalSpent == 0.00);
 
-        }     
+        $("#supports-data").append(`
+        <output> <span style= "font-weight: bold">Committee Name:</span> ${Candidate.supportersCard.support[i].committeeName}: 
+        <span style= "font-weight: bold">Total Spent:</span> ${ Candidate.supportersCard.support[i].totalSpent}</output>
+       
+        `);  
+   
 }
+$("#supports-data").append(`
 
-// //for(var j = 0; j < Candidate.supportersCard.oppose.length; j++) {
-//     if(Candidate.supportersCard.oppose[j].committeeName.totalSpent == 0.00) {
+<output><span style= "font-weight: bold">Total Support Expense:</span> ${Candidate.supportersCard.totalSupportExpense}</output>
+`);  
+for(var j = 0; j < Candidate.supportersCard.oppose.length; j++) {
+$("#supports-data").append(`
+<output><span style= "font-weight: bold">Oppose:</span> ${Candidate.supportersCard.oppose[j].committeeName}</output>
+<output><span style= "font-weight: bold">Total Spent</span> ${Candidate.supportersCard.oppose[j].totalSpent}</output>
 
-//     }
-//      else {
-//         (Candidate.supportersCard.oppose[j].committeeName. ==);
-
-//         }     
-// }
-
-
-$("#supporters-card").append(`
-
-<div class="collapsible-header" style="font-weight:bold"><span style="font-weight:bold">Title:\u00A0${supportersCard}</span></div>
-<div class="collapsible-body">    
-
-<p><span style="font-weight:bold">Support:\u00A0</span>${Candidate.supportersCard.support[i].committeeName}</p>
-<p><span style="font-weight:bold">Oppose:\u00A0</span>${Candidate.supportersCard.oppose[j].committeeName}</p>
-
-<p><span style="font-weight:bold">Total Spent:\u00A0</span>${Candidate.supportersCard.support[i].totalSpent}</p>
-<p><span style="font-weight:bold">Total Spent:\u00A0</span>${Candidate.supportersCard.oppose[j].totalSpent}</p>
-</div>
-`);
+`);      
 }
-displaySupporters();
+$("#supports-data").append(`
+<output><span style= "font-weight: bold">Total Oppose Expense</span> ${Candidate.supportersCard.totalOpposeExpense}</output>
+`); 
+ }
